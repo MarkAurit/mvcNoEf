dd8ea134bac383f2da2e35d07ba3b3b289088bf1
Mark Aurit <mark.aurit@outlook.com>
2014-08-26 15:01:40 -0700

    add with static classes implemented by controller

---
+++ Mvc4NoEF/Models/IMovieRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mvc4NoEF.Models
+{
+    interface IMovieRepository
+    {
+        int MovieID { get; set; }
+        string Name { get; set; }
+        string Email { get; set; }
+        bool FamilyFriendly { get; set; }
+        string Abstract { get; set; }
+        Movie Get(int? id);
+        void Save(Movie movie);
+        IEnumerable<Movie> GetMovies();
+        //void Movie();
+    }
+}
---
+++ Mvc4NoEF/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Mvc4NoEF.Startup))]
+namespace Mvc4NoEF
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
